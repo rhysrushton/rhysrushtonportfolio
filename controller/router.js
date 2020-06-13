ac3f5728b616e2851e0d5bbeb6d6247b706ba8f3
Rhys Rushton <rhys.rushton@gmail.com>
2020-06-13 13:45:46 +1000

    routing

--- controller/router.js
+++ controller/router.js
@@ -6,5 +6,26 @@ app.get("/", function(req, res) {
 
     res.sendFile(path.join(__dirname, "../index.html"));
   });
+
+  app.get("/contact", function(req, res) {
+    // If the user already has an account send them to the members page
+
+    res.sendFile(path.join(__dirname, "../contact.html"));
+  });
+
+  app.get("/portfolio", function(req, res) {
+    // If the user already has an account send them to the members page
+
+    res.sendFile(path.join(__dirname, "../portfolio.html"));
+  });
+
+  app.get("/things", function(req, res) {
+    // If the user already has an account send them to the members page
+
+    res.sendFile(path.join(__dirname, "../things.html"));
+  });
+
+
+
 }; 
  
